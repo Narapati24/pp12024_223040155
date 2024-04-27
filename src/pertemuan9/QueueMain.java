59234cf5e0a2306653170007c11b52230944acf4
Narapati24 <narapati.223040155@mail.unpas.ac.id>
2024-04-27 19:36:26 +0700

    pertemuan 9

---
+++ src/pertemuan9/QueueMain.java
@@ -0,0 +1,47 @@
+package pertemuan9;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        StruktureQueue queue = new StruktureQueue();
+        System.out.println("### Sebelum Enqueue ###");
+        System.out.println("Size: " + queue.size());
+        System.out.println("isEmpty: " + queue.isEmpty());
+
+        for (int i = 0; i < 3; i++){
+            queue.enqueue(3);
+        }
+
+        System.out.println("\n### Enqueue 3x ###");
+        System.out.println("Size: " + queue.size());
+        System.out.println("isEmpty: " + queue.isEmpty());
+        System.out.println("Front: " + queue.front());
+
+        queue = new StruktureQueue();
+
+        System.out.println("\n### Sebelum Enqueue 4x ###");
+        System.out.println("Size: " + queue.size());
+        System.out.println("isEmpty: " + queue.isEmpty());
+        System.out.print("ElementQueue: ");
+        queue.displayElements();
+
+        queue.enqueue(2);
+        queue.enqueue(7);
+        queue.enqueue(6);
+        queue.enqueue(1);
+
+        System.out.println("\n\n### Enqueue 4x ###");
+        System.out.println("Size: " + queue.size());
+        System.out.println("isEmpty: " + queue.isEmpty());
+        System.out.print("ElementQueue: ");
+        queue.displayElements();
+        System.out.println("\nFront: " + queue.front());
+
+        queue.dequeue();
+        System.out.println("\n### Dequeue 1x ###");
+        System.out.println("Size: " + queue.size());
+        System.out.println("isEmpty: " + queue.isEmpty());
+        System.out.print("ElementQueue: ");
+        queue.displayElements();
+        System.out.println("\nFront: " + queue.front());
+    }
+}
---
+++ src/pertemuan9/StruktureQueue.java
@@ -0,0 +1,73 @@
+package pertemuan9;
+
+public class StruktureQueue {
+    private Node FRONT, REAR;
+    public void enqueue(int data){
+        Node newNode = new Node(data);
+        if (isEmpty()){
+            FRONT = newNode;
+            REAR = newNode;
+        } else {
+            REAR.setNext(newNode);
+            REAR = newNode;
+        }
+    }
+
+//    public int dequeue(){
+//        Node temp;
+//        int dequeueItem = Integer.MIN_VALUE;
+//        if (FRONT != null){
+//            if (FRONT == REAR){
+//                dequeueItem = FRONT.getData();
+//                FRONT = null;
+//                REAR = null;
+//            } else {
+//                temp = FRONT;
+//                dequeueItem = temp.getData();
+//                FRONT = FRONT.getNext();
+//            }
+//        }
+//        return dequeueItem;
+//    }
+
+    public void dequeue(){
+        if (isEmpty()){
+            System.out.println("Data Masih Kosong");
+        } else {
+            Node tempFRONT = FRONT;
+            FRONT = FRONT.getNext();
+            REAR = null;
+            tempFRONT = null;
+        }
+    }
+
+    public int size(){
+        int size = 0;
+        Node curNode = FRONT;
+        while (curNode != null){
+            size++;
+            curNode = curNode.getNext();
+        }
+        return size;
+    }
+
+    public void displayElements(){
+        Node temp = FRONT;
+        if (FRONT != null) {
+            for (int i = 0; i < size(); i++) {
+                System.out.print(temp.getData() + " ");
+                temp = temp.getNext();
+            }
+        } else {
+            System.out.print("Queue Kosong");
+        }
+    }
+
+    public int front(){
+        return FRONT != null ? FRONT.getData() : -1;
+    }
+
+    public boolean isEmpty(){
+        return FRONT == null;
+    }
+}
