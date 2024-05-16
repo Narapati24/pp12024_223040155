d254b74dffbbd274bece5e7685782665f31c3de1
Narapati24 <narapati.223040155@mail.unpas.ac.id>
2024-05-16 16:30:41 +0700

    pertemuan 12

---
+++ src/pertemuan12/GraphMain.java
@@ -0,0 +1,34 @@
+package pertemuan12;
+
+public class GraphMain {
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+        graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addVertex('C');
+        graph.addVertex('D');
+        graph.addVertex('E');
+
+        graph.addEdge(0,1);
+        graph.addEdge(1,2);
+        graph.addEdge(0,3);
+        graph.addEdge(3,4);
+
+        graph.adjacencyMatrix();
+        System.out.println();
+        System.out.println();
+
+        graph = new Graph(4);
+        graph.addVertex('A'); //0
+        graph.addVertex('B'); //1
+        graph.addVertex('C'); //2
+        graph.addVertex('D'); //3
+
+        graph.addEdge(0,1);
+        graph.addEdge(0,2);
+        graph.addEdge(0,3);
+        graph.addEdge(1,3);
+
+        graph.adjacencyMatrix();
+    }
+}
---
+++ src/pertemuan12/Vertex.java
@@ -0,0 +1,26 @@
+package pertemuan12;
+
+public class Vertex {
+    private char label;
+    private boolean wasVisited;
+    public Vertex(char lab){
+        label = lab;
+        wasVisited = false;
+    }
+
+    public char getLabel() {
+        return label;
+    }
+
+    public void setLabel(char label) {
+        this.label = label;
+    }
+
+    public boolean isWasVisited() {
+        return wasVisited;
+    }
+
+    public void setWasVisited(boolean wasVisited) {
+        this.wasVisited = wasVisited;
+    }
+}
